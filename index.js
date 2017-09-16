8377b40dc95257558183c5ea8c7848bd12b5e20e
gwimbrow <graham.wimbrow@gmail.com>
2017-09-16 17:20:32 -0400

    WIP

--- index.js
+++ index.js
@@ -2,40 +2,28 @@ import "babel-polyfill";
 
 import componentConfig from "./config/components";
 
-export * from "./modules";
+import Wrapper from "./lib/core/Wrapper";
+
+import {mapObj} from "./lib/util";
+
+const context = require.context("./lib/components", true, /\.js$/);
+
+const components = context.keys().reduce((obj, key) => {
+
+    let name = key.replace(/(^\.(\/|\\|\:)|\..*js$)/g, "");
+
+    return Object.assign(obj, {
+        [name]: context(key).default
+    });
+}, {});
 
 export const register = new Map();
-
-// describe the component get/set-accessor pattern for observed properties
-function setDefault (node, key, conf) {
-
-    return !!Object.defineProperty(this, key, {
-
-        get () { return conf.value },
-        
-        set (val) {
-            // components can reject an update
-            if (conf.writable && this.willUpdate(key, val)) {
-                // assign the value to the node, and update the confif
-                conf.value = val;
-                node[key] = val;
-
-                if (this.isMounted) {
-                    // if it is mounted, re-mount the component's node
-                    this.node.innerHTML = this.mount();
-                }
-
-                return this.didUpdate(key, val);
-            }
-        }
-    });
-}
 
 export function reMount (src, key, val) {
 
     const group = `${src.constructor.name}.${key}`;
 
-    const subscribers = mslto.register.get(group).slice();
+    const subscribers = register.get(group).slice();
 
     while (subscribers.length) {
 
@@ -43,81 +31,84 @@ export function reMount (src, key, val) {
 
         target[key] = val;
     }
-
-    return src.didMount();
 }
 
-function valuesFor (conf) {
+function findSource (target, key) {
 
-    return Object.keys(conf).reduce((defs, prop) => {
-
-        return Object.assign(defs, {
-            [prop]: {
-                value: conf[prop]
-            }
-        });
-    }, {});
-}
-
-function findSource (target) {
-    // else return falsish undefined
-    if (target.hasOwnProperty(key)) {
+    if (target && target.hasOwnProperty(key)) {
 
         let src = target;
-        // src is undefined if parent is missing
-        while (src && src.hasOwnProperty(key)) {
 
-            src = src.parent;
+        while (src.hasOwnProperty("parent")) {
+
+            let parent = src.parent;
+
+            if (parent.hasOwnProperty(key)) {
+
+                src = parent;
+
+            } else {
+
+                break;
+            }
         }
+
         return src;
     }
+
+    return false;
 }
 
 function getAccessorTrap (target, key) {
 
     if (!target.hasOwnProperty(key)) {
 
-        let source = findSource(target);
+        let source = findSource(target.parent, key);
 
         if (source) {
             
             let group = `${source.constructor.name}.${key}`;
 
-            let subscribers = mslto.register.get(group);
+            let subscribers = register.get(group);
 
             target.setDefault(key, source[key]);
 
-            if (subscribers) {
-
-                subscribers.push(target);
-
-            } else {
-
-                mslto.register.set(group, [target]);
-            }
+            subscribers.push(target);
         }
     }
 
     return target[key];
 }
 
-function setAccessorTrap (target, key, val) {
+function setAccessorTrap (target, config, val) {
 
-    if (!target.hasOwnProperty(key)) {
+    const key = typeof config === "string" ? config : config.value;
 
-        target.setDefault(key, { value: val });
+    const source = findSource(target, key);
+
+    if (source) {
+
+        mslto.reMount(source, key, val);
 
     } else {
+        // this is the genesis of a new subscribers group
+        let group = `${target.constructor.name}.${key}`;
 
-        return target[key] = val;
+        console.log(target, group);
+
+        target.setDefault(key, config.value ? config : {
+
+            value: val, writable: true, configurable: true
+        });
+
+        register.set(group, [target]);
     }
 
     return true;
 }
+
 // facilitate component instantiations
 export function create (type, node, config = {}) {
-    // question - how can you detect if a function is invoked with ".call" and a bound context?
-    const parent = this;
     // JSON configuration
     let target = componentConfig[type];
     // if the configuration includes defaults
@@ -127,22 +118,28 @@ export function create (type, node, config = {}) {
         // target the component constructor
         target = target.component;
     }
-    // instantiate a new component
-    const component = new mslto[target](node);
-    // define a component proxy
-    const reflector = new Proxy(component, {
-        // use common accessor traps
-        set: setAccessorTrap,
-        get: getAccessorTrap
-    });
-    // curry setDefault
-    const setDefault = setDefault.bind(component, node);
-    // transform accessors
-    const defaults = valuesFor({node, parent, reflector, setDefault});
-    // shared component defaults are described in JSON format
-    Object.defineProperties(component, componentConfig["defaults"], defaults);
 
-    Object.keys(config).forEach(prop => setDefault(prop, config[prop]));
+    const accessors = { node };
+
+    if (this instanceof Wrapper) {
+
+        accessors.parent = this;
+    }
+
+    const rel = mapObj(accessors, (obj, key) => {
+        return {
+            [key]: { value: obj[key] }
+        };
+    });
+
+    const defaults = Object.assign(componentConfig["defaults"], rel);
+
+    const Constructor = components[target];
+
+    const component = new Constructor(defaults, config, {
+        get: getAccessorTrap,
+        set: setAccessorTrap
+    });
 
     return component;
 }
--- modules.js
+++
@@ -1,6 +0,0 @@
-// re-export this file from index.js
-// includes modules in lib/
-// do not use *default* export syntax
-// supports multiple exports from the included files
-export * from "./lib/components/test";
-export * from "./lib/components/test-child";
